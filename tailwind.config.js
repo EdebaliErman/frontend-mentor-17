554240bf336b635ac23ce2ee2391b1288166467a
EdebaliErman <edebalierman@gmail.com>
2023-11-25 18:43:13 +0300

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink:"hsl(322, 100%, 66%)",
+        paleCyan:"hsl(193, 100%, 96%)",
+        darkCyan:"hsl(192, 100%, 9%)",
+        grayBlue:"hsl(208, 11%, 55%)"
+      },
+      fontFamily:{
+        "popins":["Poppins","sans-serif"],
+        "opensans":["Open Sans","sans-serif"]
+
+      }
+    },
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
