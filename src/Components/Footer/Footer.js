ad92a981d5d4e7631c033ef799fdab3e39b716a9
EdebaliErman <edebalierman@gmail.com>
2023-11-25 23:22:06 +0300

    Yapılacak bölümler ayırıldı

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { images } from '../../Data/data'
 
 function Footer() {
   return (
     <div>
-      
+      <div>
+        <h1>Ready To Build Your Community?</h1>
+        <button>Get Started For Free</button>
+      </div>
+      <div>
+        <img className='md:w-20 ' src={images.logo} alt='logo' />
+        <div>
+          {/*------- Contact --------  */}
+          <div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          {/*------- Page About --------  */}
+          <div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          {/*------- Page İnfo --------  */}
+          <div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          {/*------- Social Media --------  */}
+          <div>
+            <span>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
