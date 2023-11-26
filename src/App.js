ee788bc1235659b3e112e4e929384077e62ed831
EdebaliErman <edebalierman@gmail.com>
2023-11-26 03:54:24 +0300

    Footer Complate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
-
-
-
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About/>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,44 +1,67 @@
 import React from 'react'
 import { images } from '../../Data/data'
+import "./Footer.css"
+
 
 function Footer() {
   return (
-    <div>
-      <div>
+    <div className='Footer'>
+      <div className='asking-card'>
         <h1>Ready To Build Your Community?</h1>
         <button>Get Started For Free</button>
       </div>
-      <div>
-        <img className='md:w-20 ' src={images.logo} alt='logo' />
-        <div>
+      <div className='contact'>
+        <img className='md:w-60 md:px-4 px-32 mb-12 ' src={images.logoWhite} alt='logo' />
+        <div className='contact-bar'>
           {/*------- Contact --------  */}
-          <div>
+          <div className='md:mb-12'>
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li >
+                <img src={images.location} />
+                <p className='w-60'>
+                  Lorem ipsum dolor sit amet, consectetur adipisang cult, sed do eiusmod tempor inadidunt ut labore et dolore magna auiqua
+                </p>
+              </li>
+              <li>
+                <img src={images.phone} />
+                <p>+1-543-123-4567</p>
+              </li>
+              <li>
+                <img src={images.email} />
+                <p>example@huddle.com</p>
+              </li>
             </ul>
           </div>
           {/*------- Page About --------  */}
-          <div>
+          <div >
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>About Us</li>
+              <li>What We Do </li>
+              <li>FAQ</li>
             </ul>
           </div>
           {/*------- Page İnfo --------  */}
-          <div>
+          <div className='mb-12'>
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Career</li>
+              <li>Blog</li>
+              <li>Contact Us</li>
             </ul>
           </div>
           {/*------- Social Media --------  */}
-          <div>
-            <span>
-            </span>
+          <div className='footer-social' >
+            <div className='social'>
+              <span className='group '>
+                {images.facebook}
+              </span>
+              <span className='group'>
+                {images.instagram}
+              </span>
+              <span className='group'>
+                {images.twitter}
+              </span>
+            </div>
+            <p className='md:text-sm '>© Copyright 2018 Huddle. All rights reserved. </p>
           </div>
         </div>
       </div>
